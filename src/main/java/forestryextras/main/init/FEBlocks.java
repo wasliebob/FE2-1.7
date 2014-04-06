95b5e1e47f4a06319346f28df7a7b83d305086e4
wasliebob <wasliebob@live.nl>
2014-04-06 20:55:34 +0200

    Removed Draconic Ore since it is not used anymore, use the crafting recipe instead

--- src/main/java/forestryextras/main/init/FEBlocks.java
+++ src/main/java/forestryextras/main/init/FEBlocks.java
@@ -6,7 +6,6 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
-import cpw.mods.fml.common.registry.GameRegistry;
 import forestry.api.recipes.RecipeManagers;
 import forestryextras.blocks.FEBlock;
 import forestryextras.blocks.FEBlockProducer;
@@ -17,17 +16,14 @@ public class FEBlocks {
 	{
 		draconicBlock = new FEBlock(Material.iron, "draconicBlock", "draconicblock", true, 1.0F, new ItemStack(FEItems.draconicIngot));
 		reinforcedBlock = new FEBlock(Material.iron, "reinforcedBlock", "reinforcedblock", true, 1.0F, new ItemStack(FEItems.reinforcedIngot));
-		draconicOre = new FEBlock(Material.iron, "draconicOre", "draconicore", false, 0F, null);
-		producer = new FEBlockProducer("Beeducer");
-		
-//		upgrader = new FEBlockUpgrader(Config.upgraderId, "Upgrader");
-		
+//		draconicOre = new FEBlock(Material.iron, "draconicOre", "draconicore", false, 0F, null);
+		producer = new FEBlockProducer("Beeducer");		
 		smelting();
 	}
 	
 	public static void smelting()
 	{
-		GameRegistry.addSmelting(draconicOre, new ItemStack(FEItems.draconicIngot), 0.0F);
+//		GameRegistry.addSmelting(draconicOre, new ItemStack(FEItems.draconicIngot), 0.0F);
 	}
 	
 	public static void crafting()
@@ -43,8 +39,6 @@ public class FEBlocks {
 	
 	public static FEBlock draconicBlock;
 	public static FEBlock reinforcedBlock;
-	public static FEBlock draconicOre;
+//	public static FEBlock draconicOre;
 	public static FEBlockProducer producer;
-
-//	public static FEBlockUpgrader upgrader;
 }
