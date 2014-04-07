563be88d8da2003f496812cfe2a9d68b84d253b5
wasliebob <wasliebob@live.nl>
2014-04-07 19:34:20 +0200

    Added Localization option for the beeducer

---
+++ src/main/java/forestryextras/helpers/LocalizationHelper.java
@@ -0,0 +1,11 @@
+package forestryextras.helpers;
+
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+public class LocalizationHelper {
+
+	public static String getLocalization(String name, int line)
+	{
+		return LanguageRegistry.instance().getStringLocalization(name + "." + line);
+	}
+}
